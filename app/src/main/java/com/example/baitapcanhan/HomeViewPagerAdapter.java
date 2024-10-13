7a3b4c30bb671dba983bd4be861c613be8b2aa98
TNQuangThao <thaotruong23082004@gmail.com>
2024-10-13 23:46:04 +0700

    QuangThao

--- app/src/main/java/com/example/baitapcanhan/HomeViewPagerAdapter.java
+++ app/src/main/java/com/example/baitapcanhan/HomeViewPagerAdapter.java
@@ -1,14 +1,14 @@
 package com.example.baitapcanhan;
 
-import android.view.FrameStats;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-public class ViewPagerAdapter extends FragmentStateAdapter {
-    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+import java.util.Set;
+
+public class HomeViewPagerAdapter extends FragmentStateAdapter {
+    public HomeViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
         super(fragmentActivity);
     }
 
@@ -16,10 +16,10 @@ public class ViewPagerAdapter extends FragmentStateAdapter {
     @Override
     public Fragment createFragment(int position) {
        switch (position){
-           case 0: return new Tab1Fragment();
-           case 1: return new Tab2Fragment();
-           case 2: return new Tab3Fragment();
-           default: return new Tab1Fragment();
+           case 0: return new HomeFragment();
+           case 1: return new ProfileFragment();
+           case 2: return new SettingFragment();
+           default: return new HomeFragment();
        }
     }
 
--- app/src/main/java/com/example/baitapcanhan/MainActivity.java
+++ app/src/main/java/com/example/baitapcanhan/MainActivity.java
@@ -8,9 +8,6 @@ import android.widget.FrameLayout;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -18,74 +15,81 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayout.OnTabSelectedListener;
-
 public class MainActivity extends AppCompatActivity {
-    TabLayout tabLayout;
-    ViewPager2 viewPager2;
-    ViewPagerAdapter viewPagerAdapter;
+
     BottomNavigationView bottomNavigationView;
-    FrameLayout frameLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        tabLayout=findViewById(R.id.tabLayout);
-        viewPagerAdapter=new ViewPagerAdapter(this);
-        viewPager2=findViewById(R.id.viewPager);
-        viewPager2.setAdapter(viewPagerAdapter);
-        bottomNavigationView=findViewById(R.id.bottomNav);
-        frameLayout=findViewById(R.id.frameLayout);;
-        tabLayout.addOnTabSelectedListener(new OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager2.setVisibility(View.VISIBLE);
-                frameLayout.setVisibility(View.GONE);
-                viewPager2.setCurrentItem(tab.getPosition());
-            }
+        bottomNavigationView = findViewById(R.id.bottomNav);
 
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-            }
+        // Mặc định load fragment Home
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragment())
+                .commit();
 
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-                viewPager2.setVisibility(View.VISIBLE);
-                frameLayout.setVisibility(View.GONE);
-            }
-        });
-        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageSelected(int position) {
-                switch (position){
-                    case 0:
-                    case 1:
-                    case 2:
-                        tabLayout.getTabAt(position).select();
-                }
-                super.onPageSelected(position);
-            }
-        });
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                frameLayout.setVisibility(View.VISIBLE);
-                viewPager2.setVisibility(View.GONE);
-                if(item.getItemId()==R.id.menu_home){
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            if(item.getItemId()==R.id.menu_home){
                     getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.frameLayout,new HomeFragment()).commit();
+                            .replace(R.id.fragment_container,new HomeFragment()).commit();
                 }else if(item.getItemId()==R.id.menu_profile){
                     getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.frameLayout,new ProfileFragment()).commit();
+                            .replace(R.id.fragment_container,new ProfileFragment()).commit();
                 }else if(item.getItemId()==R.id.menu_setting){
                     getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.frameLayout,new SettingFragment()).commit();
+                            .replace(R.id.fragment_container,new SettingFragment()).commit();
                 }else{
                     return false;
                 }
-                return true;
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
             }
+
+            return true;
         });
     }
 }
+
+//public class MainActivity extends AppCompatActivity {
+//    TabLayout tabLayout;
+//    ViewPager2 viewPagerHome;
+//    HomeViewPagerAdapter viewPagerAdapter;
+//    BottomNavigationView bottomNavigationView;
+//    FrameLayout frameLayout;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_main);
+//
+//        viewPagerAdapter=new HomeViewPagerAdapter(this);
+//        viewPagerHome=findViewById(R.id.viewPagerHome);
+//        viewPagerHome.setAdapter(viewPagerAdapter);
+//        bottomNavigationView=findViewById(R.id.bottomNav);
+//        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                if(item.getItemId()==R.id.menu_home){
+//                    getSupportFragmentManager().beginTransaction()
+//                            .replace(R.id.viewPagerHome,new HomeFragment()).commit();
+//                }else if(item.getItemId()==R.id.menu_profile){
+//                    getSupportFragmentManager().beginTransaction()
+//                            .replace(R.id.viewPagerHome,new ProfileFragment()).commit();
+//                }else if(item.getItemId()==R.id.menu_setting){
+//                    getSupportFragmentManager().beginTransaction()
+//                            .replace(R.id.viewPagerHome,new SettingFragment()).commit();
+//                }else{
+//                    return false;
+//                }
+//                return true;
+//            }
+//        });
+//    }
+//}
